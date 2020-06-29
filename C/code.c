589391b1e853f17887d85d8e70c9f369d89be77f
datt16@desktop <osatom.4639@gmail.com>
2020-06-29 22:07:30 +0900

    2020-6-29分の演習を追加

--- C/code.c
+++ C/code.c
@@ -1,41 +1,13 @@
-/*
-  10進数を2進数に変換
-*/
-
+// Example 07-05
 #include <stdio.h>
-
-int main(void)
+#include <stdlib.h>
+int main(int argc, char *argv[])
 {
-    char retry; //もう一度やるかの確認//
-    char n = 'n';
-
-    do
-    {          //もう一度やるための繰り返し//
-        int j; //10進数を入れる//
-        int z; //keke//
-        int i[32];
-        ;
-
-        printf("10進数(0～9999)を入力してください:");
-        scanf_s("%d", &j);
-        if (j >= 0 && j <= 9999)
-        {
-            for (z = 0; j > 0; z++)
-            {
-                i[z] = j % 2;
-                j = j / 2;
-            }
-            while (z > 0)
-                printf("%1d", i[--z]);
-            printf("\n");
-        }
-        else
-        {
-            puts("正しい値を入力してください");
-        }
-        printf("終了しますか？ y/n");
-        scanf_s(" %c", &retry);
-    } while (retry == n);
+    for (int i = 0; i < argc; i++)
+    {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    printf("%s+%s = %d\n", argv[1], argv[2], atoi(argv[1]) + atoi(argv[2]));
 
     return 0;
 }
